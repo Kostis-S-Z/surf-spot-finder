f53eca92816ad3c3bb1fffa201fe169c3c8297e2
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2025-03-19 10:36:54 +0100

    7 make tools configurable (#21)
    * Make tools configurable via arguments.
    
    - Make `mcp` optional, not used by default.
    - Update examples to use `tools`.
    
    * Update pyproject.tooml optional deps
    
    * Make openai and smolagents imports lazy
    
    * Update api.md with sections
    
    * fix tests
    
    * Add missing arize

--- src/surf_spot_finder/agents/smolagents.py
+++ src/surf_spot_finder/agents/smolagents.py
@@ -1,16 +1,21 @@
+import importlib
 import os
 from typing import Optional
 
 from loguru import logger
 
-from smolagents import (
-    CodeAgent,
-    DuckDuckGoSearchTool,
-    LiteLLMModel,
-    ToolCollection,
-)
-from mcp import StdioServerParameters
 from surf_spot_finder.prompts.smolagents import SYSTEM_PROMPT
+
+try:
+    from smolagents import (
+        CodeAgent,
+        LiteLLMModel,
+        ToolCollection,
+    )
+
+    smolagents_available = True
+except ImportError:
+    smolagents_available = None
 
 
 @logger.catch(reraise=True)
@@ -19,6 +24,7 @@ def run_smolagent(
     prompt: str,
     api_key_var: Optional[str] = None,
     api_base: Optional[str] = None,
+    tools: Optional[list[str]] = None,
 ) -> CodeAgent:
     """
     Create and configure a Smolagents CodeAgent with the specified model.
@@ -39,6 +45,26 @@ def run_smolagent(
         >>> agent = run_smolagent("anthropic/claude-3-haiku", "my prompt here", "ANTHROPIC_API_KEY", None, None)
         >>> agent.run("Find surf spots near San Diego")
     """
+    if not smolagents_available:
+        raise ImportError("You need to `pip install smolagents` to use this agent")
+
+    if tools is None:
+        tools = [
+            "smolagents.DuckDuckGoSearchTool",
+            "smolagents.VisitWebpageTool",
+            "smolagents.PythonInterpreterTool",
+        ]
+
+    imported_tools = []
+    mcp_tool = None
+    for tool in tools:
+        if "mcp" in tool:
+            mcp_tool = tool
+        else:
+            module, func = tool.rsplit(".", 1)
+            module = importlib.import_module(module)
+            tool = getattr(module, func)
+            imported_tools.append(tool())
 
     model = LiteLLMModel(
         model_id=model_id,
@@ -46,24 +72,31 @@ def run_smolagent(
         api_key=os.environ[api_key_var] if api_key_var else None,
     )
 
-    # We could easily use any of the MCPs at https://github.com/modelcontextprotocol/servers
-    # or at https://glama.ai/mcp/servers
-    # or at https://smithery.ai/
-    server_parameters = StdioServerParameters(
-        command="docker",
-        args=["run", "-i", "--rm", "mcp/fetch"],
-        env={**os.environ},
-    )
-    # https://huggingface.co/docs/smolagents/v1.10.0/en/reference/tools#smolagents.ToolCollection.from_mcp
-    with ToolCollection.from_mcp(server_parameters) as tool_collection:
+    if mcp_tool:
+        from mcp import StdioServerParameters
+
+        # We could easily use any of the MCPs at https://github.com/modelcontextprotocol/servers
+        # or at https://glama.ai/mcp/servers
+        # or at https://smithery.ai/
+        server_parameters = StdioServerParameters(
+            command="docker",
+            args=["run", "-i", "--rm", mcp_tool],
+            env={**os.environ},
+        )
+        # https://huggingface.co/docs/smolagents/v1.10.0/en/reference/tools#smolagents.ToolCollection.from_mcp
+        with ToolCollection.from_mcp(server_parameters) as tool_collection:
+            agent = CodeAgent(
+                tools=imported_tools + tool_collection.tools,
+                prompt_templates={"system_prompt": SYSTEM_PROMPT},
+                model=model,
+                add_base_tools=False,  # Turn this on if you want to let it run python code as it sees fit
+            )
+            agent.run(prompt)
+    else:
         agent = CodeAgent(
-            tools=[
-                *tool_collection.tools,
-                DuckDuckGoSearchTool(),
-            ],
+            tools=imported_tools,
             prompt_templates={"system_prompt": SYSTEM_PROMPT},
             model=model,
-            add_base_tools=False,  # Turn this on if you want to let it run python code as it sees fit
         )
         agent.run(prompt)
 
